5e03336c9cb1d7480c468291301554f7bf78b466
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-14 23:19:34 +0400

    add rand

---
+++ guessing_game/target/debug/deps/cfg_if-64bb2cffd428e39a.d
@@ -0,0 +1,7 @@
+C:\Users\cetin\Desktop\Rust\guessing_game\target\debug\deps\libcfg_if-64bb2cffd428e39a.rmeta: C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\cetin\Desktop\Rust\guessing_game\target\debug\deps\libcfg_if-64bb2cffd428e39a.rlib: C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\cetin\Desktop\Rust\guessing_game\target\debug\deps\cfg_if-64bb2cffd428e39a.d: C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs:
---
+++ guessing_game/target/debug/deps/zerocopy_derive-39bc29f58e9dc54e.d
@@ -0,0 +1,7 @@
+C:\Users\cetin\Desktop\Rust\guessing_game\target\debug\deps\zerocopy_derive-39bc29f58e9dc54e.dll: C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\lib.rs C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\ext.rs C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\repr.rs
+
+C:\Users\cetin\Desktop\Rust\guessing_game\target\debug\deps\zerocopy_derive-39bc29f58e9dc54e.d: C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\lib.rs C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\ext.rs C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\repr.rs
+
+C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\lib.rs:
+C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\ext.rs:
+C:\Users\cetin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\zerocopy-derive-0.7.35\src\repr.rs:
