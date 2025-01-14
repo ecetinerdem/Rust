4926b305d83c7925ab121e80f03581c61722018c
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-14 22:30:05 +0400

    create guessing game

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+C:\Users\cetin\Desktop\Rust\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+C:\Users\cetin\Desktop\Rust\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\cetin\Desktop\Rust\guessing_game\target\debug\guessing_game.exe: C:\Users\cetin\Desktop\Rust\guessing_game\src\main.rs
