32e6bb401986db84ee98075c1730ab1ac70e4564
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-14 21:57:07 +0400

    add cargo build

---
+++ hello_cargo/target/debug/deps/hello_cargo-722d6fb0054e899e.d
@@ -0,0 +1,5 @@
+C:\Users\cetin\Desktop\Rust\hello_cargo\target\debug\deps\libhello_cargo-722d6fb0054e899e.rmeta: src\main.rs
+
+C:\Users\cetin\Desktop\Rust\hello_cargo\target\debug\deps\hello_cargo-722d6fb0054e899e.d: src\main.rs
+
+src\main.rs:
