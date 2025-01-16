d866eab62fae8181b7900b743ddac50fc52b7069
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-16 21:53:59 +0400

    statement and expresion

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -21,6 +21,12 @@ fn main() {
     another_function(5);
 
     print_labeled_measurement(5, 'h');
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
 }
 
 fn another_function(x: i32) {
