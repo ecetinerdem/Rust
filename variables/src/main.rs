0879bc60e63d428d8b107823fa7bc9bf7a686317
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-16 21:47:23 +0400

    add fn argument

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -19,8 +19,14 @@ fn main() {
     println!("The value of the element at index {index} is: {element}");
 
     another_function(5);
+
+    print_labeled_measurement(5, 'h');
 }
 
 fn another_function(x: i32) {
     println!("The value of x: {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}")
 }
