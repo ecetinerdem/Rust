df8a4271f18de5c931985b10a2ee514c49d44cff
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-16 22:48:20 +0400

    Update main.rs

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -18,3 +18,17 @@ fn nmthree() {
         println!("num is something other than zero");
     }
 }
+
+fn mltcond() {
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+}
