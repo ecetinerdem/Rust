1d534f4782fec677ce4eb6077227ca693aa9355b
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-16 22:46:14 +0400

    Update main.rs

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -7,3 +7,14 @@ fn main() {
         println!("condition was false");
     }
 }
+
+fn nmthree() {
+    let num = 3;
+    if num == 3 {
+        println!("num was three");
+    }
+
+    if num != 0 {
+        println!("num is something other than zero");
+    }
+}
