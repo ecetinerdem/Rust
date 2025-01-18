8cf546832bd1a9baa4d101dc1c8051914d73d67f
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-18 23:41:00 +0400

    ownership

---
+++ ownership/target/debug/deps/ownership.d
@@ -0,0 +1,5 @@
+C:\Users\cetin\Desktop\Rust\ownership\target\debug\deps\ownership.exe: src\main.rs
+
+C:\Users\cetin\Desktop\Rust\ownership\target\debug\deps\ownership.d: src\main.rs
+
+src\main.rs:
