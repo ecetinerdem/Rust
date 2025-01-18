cc6d938ba219bd26cdeea423dee39fc74b5319d9
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-19 01:25:36 +0400

    Update main.rs

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -1,8 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+    let my_string = String::from("hello world");
+
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    let word = first_word(&my_string);
+
+    //----------------------------------------
+
+    let my_string_literal = "hello world";
+
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+    let word = first_word(&my_string_literal);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
