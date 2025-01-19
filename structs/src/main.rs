243a2f9809f84ea083daee578a8e2409dbe50077
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-19 21:57:40 +0400

    add example

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -8,8 +8,28 @@ fn main() {
 
     let user1 = User {
         active: true,
-        name: String::from("Arzu"),
+        username: String::from("someone123"),
         email: String:: from("Bxu5f@example.com"),
         sign_in_count: 1,
     }
+
+    let mut user2 = User {
+        active: true,
+        username: String::from("someone321"),
+        email: String:: from("someone321@example.com"),
+        sign_in_count: 1,
+    }
+
+    
 }
+
+fn build_user (email: String, username: String) -> User {
+    User {
+        active: false,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+
