58a7a9f7242e0220d58aa8bcf64ed56dc8fc910d
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-19 22:20:37 +0400

    add area calculate

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -6,8 +6,38 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(width1, height1)
     )
+
+    let rect1 = (30,50);
+    println!("The area of the rectangle is {} square pixels.", area_calculate(rect1));
+
+    let rect2 = Rectangle {
+        width: 30,
+        height: 50,
+    }
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        calculate_area(&rect2)
+    )
 }
 
 fn area(width: u32, height: u32) -> u32 {
     width * height
+}
+
+
+fn area_calculate(dimensions: (u32, u32)) -> u32 {
+    dimensions.0 * dimensions.1
+}
+
+
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+
+fn calculate_area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
