a59fa7246812084393508dd8c0ce6927d4e08c20
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-19 23:20:26 +0400

    structs and rectangle

--- method/src/main.rs
+++ method/src/main.rs
@@ -8,6 +8,14 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+    
+    fn can_hold(&self, other_rect: &Rectangle) -> bool {
+        self.width > other_rect.width && self.height > other_rect.height
+    }
 }
 
 fn main() {
@@ -16,8 +24,34 @@ fn main() {
         height: 50,
     };
 
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    }
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    }
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
     println!(
         "The area of the rectangle is {} square pixels.",
         rect1.area()
     );
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
 }
+
+
+
+
+
+
+
+
+
