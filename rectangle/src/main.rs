e691323e984e6828746e18f1d8075b016cb0b44d
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-19 22:32:00 +0400

    update rectangle struct

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -19,6 +19,8 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         calculate_area(&rect2)
     )
+
+    println!("rect2 is {:#?}" rect2);
 }
 
 fn area(width: u32, height: u32) -> u32 {
@@ -31,7 +33,7 @@ fn area_calculate(dimensions: (u32, u32)) -> u32 {
 }
 
 
-
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
