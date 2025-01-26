d7ec209e073f797a6d610b5315b4494de57060b5
Cetin Erdem <cetoerdem1@gmail.com>
2025-01-26 23:51:54 +0400

    add coins

---
+++ mtchexprs/target/debug/deps/mtchexprs.d
@@ -0,0 +1,5 @@
+C:\Users\cetin\Desktop\Rust\mtchexprs\target\debug\deps\mtchexprs.exe: src\main.rs
+
+C:\Users\cetin\Desktop\Rust\mtchexprs\target\debug\deps\mtchexprs.d: src\main.rs
+
+src\main.rs:
---
+++ mtchexprs/target/debug/mtchexprs.d
@@ -0,0 +1,1 @@
+C:\Users\cetin\Desktop\Rust\mtchexprs\target\debug\mtchexprs.exe: C:\Users\cetin\Desktop\Rust\mtchexprs\src\main.rs
