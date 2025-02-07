f1d2f67e752c61b31a272585143f1a6cf395f8c0
Cetin Erdem <cetoerdem1@gmail.com>
2025-02-07 23:16:56 +0400

    add details

--- cliapp/src/main.rs
+++ cliapp/src/main.rs
@@ -4,14 +4,26 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
+    let config = Config::new(&args);
 
-    let filename = &args[2];
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
-
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    let contents =
+        fs::read_to_string(config.filename).expect("Something went wrong reading the file");
 
     println!("With text: \n{}", contents);
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
 }
